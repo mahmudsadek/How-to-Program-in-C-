71f9d74de7ee8615b47bc30bfc7ec44e8a0b8f20
Mahmoud Sadek <101598718+mahmudsadek@users.noreply.github.com>
2023-01-31 17:18:49 +0200

    Add files via upload

---
+++ Chapter 1/2-16.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std ;
+int main () {
+    int number1;
+    int number2;
+
+    cin >> number1 >> number2 ;
+
+    cout << number1+number2 << endl;
+    cout << number1*number2 << endl;
+    cout << number1-number2 << endl;
+    cout << number1/number2 << endl;
+} 
